5bcbeabac15e3e22e1a66388628a31ff0cfdf0e3
fabiomartinsbrrj <fabiomartinsbr@gmail.com>
2025-05-31 16:24:53 -0300

    feat: iam roles to controlplane and nodes

---
+++ ksm.tf
@@ -0,0 +1,13 @@
+resource "aws_kms_key" "main" {
+  description = "KMS key for ${var.project_name} EKS cluster"
+
+  tags = {
+    Name = "${var.project_name}-eks-kms-key"
+  }
+  
+}
+
+resource "aws_kms_alias" "main" {
+  name          = "alias/${var.project_name}-eks-kms-key"
+  target_key_id = aws_kms_key.main.key_id
+}
